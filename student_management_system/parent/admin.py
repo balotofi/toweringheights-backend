8312e86e7273830ef85fcc8b7ec8070c31504814
drvnprgrmr <ekunweibukun159357@gmail.com>
2022-08-04 19:47:19 +0100

    Added parent model

--- student_management_system/parent/admin.py
+++ student_management_system/parent/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Parent, Family
+
+admin.site.register([Parent, Family])
--- student_management_system/parent/models.py
+++ student_management_system/parent/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Parent(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    sex = models.CharField(
+        max_length=1,
+        choices=[
+            ("M", "Male"),
+            ("F", "Female")
+        ]
+    )
+
+
+
+class Family(models.Model):
+    father = models.ForeignKey(Parent, on_delete=models.PROTECT, related_name="fathers")
+    mother = models.ForeignKey(Parent, on_delete=models.PROTECT, related_name="mothers")
+    wards = models.ManyToManyField("student.Student")
+
+    class Meta:
+        verbose_name_plural = "Families"
+
